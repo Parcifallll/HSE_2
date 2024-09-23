00eff48117cb2b51aeed09aa6507272269351ee5
Parcifallll <email@example.com>
2024-09-23 19:01:41 +0300

    Enter numbers

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, world!");
+        System.out.println("Enter a number:");
+        int a = scanner.nextInt();
+        System.out.println("Enter a number:");
+        int b = scanner.nextInt();
+        scanner.close();
+
     }
 }
